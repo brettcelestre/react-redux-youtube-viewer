de2bbe75e6e0a5ff52a65c88c95005a4b5fffcbe
Brett Celestre <bcelestre@gmail.com>
2017-02-27 15:15:16 -0800

    Adds styles and links search bar up to API call

--- src/index.js
+++ src/index.js
@@ -17,8 +17,12 @@ class App extends Component {
       selectedVideo: null
     };
     
+    this.videoSearch('surfboards')
+  }
+  
+  videoSearch(term) {
     YTSearch(
-      {key: API_KEY, term: 'surfboards'}, videos => {
+      {key: API_KEY, term: term}, videos => {
         // console.log('videos: ', videos);
         this.setState({ 
           videos: videos,
@@ -30,7 +34,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList 
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
